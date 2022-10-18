3850ae5ef3a4d97d908791593915cf6c97b349a5
unknown <muelinx@gmail.com>
2022-10-18 08:56:02 +0100

    feat: setting up my portfolio website

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./NavBar.css"
+function NavBar() {
+  return (
+    <div>
+<div class="div1">
+        <nav class="navbar">
+           <ul>
+               <li>Home</li>
+               <li>About</li>
+               <li>Resume</li>
+               <li>Works</li>
+               <li>Testimonials</li>
+               <li>Say Hello</li>
+           </ul>      
+        </nav>
+
+     </div>
+     <div className='div2'>
+           <h1> I'm Muelji Damos, aka MJ.</h1>
+           <h3> I'm an <b> Front-End Developer,</b> Chemical Engineer, a <b>visionary</b> and a creative, who has the sole passion of creating 
+               effective an optimal visual identities for individuals, companies and organizations of all sizes around the world.
+                Start scrolling and learn more about me and my spectacular works.
+            </h3>           
+     
+                    <hr/> <br/> <br/>
+
+            <div>
+
+
+
+
+            </div>
+    
+    
+    </div>
+     
+
+
+    </div>
+  )
+}
+
+export default NavBar
