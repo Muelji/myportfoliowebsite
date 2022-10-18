a8eca5f3eb0efa4dba46442a1527a398617c6cc1
unknown <muelinx@gmail.com>
2022-10-18 16:48:33 +0100

    feat: added the about me component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import AboutMe from './Components/AboutMe';
 import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div>
     <NavBar/>
+    <AboutMe/>
     </div>
   );
 }
---
+++ src/Components/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./AboutMe.css"
+
+
+function Aboutme() {
+  return (
+    <div>Aboutme</div>
+  )
+}
+
+export default Aboutme
