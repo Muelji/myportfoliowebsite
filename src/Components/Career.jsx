f62553425c8f8445b30a95100adddab5de145270
unknown <muelinx@gmail.com>
2022-10-20 17:24:14 +0100

    working on the career component

--- src/Components/Career.jsx
+++ src/Components/Career.jsx
@@ -5,6 +5,17 @@ function Career() {
   return (
     <div>
 
+         <div>
+            <h3 id="hcar">CAREER</h3>
+              <div class="divcari"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero explicabo accusamus, quisquam aliquam obcaecati excepturi quam aperiam blanditiis neque laborum unde. Fugit commodi dicta impedit saepe dolore, doloremque repellendus dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt eius modi debitis fuga sequi dicta quisquam dolorum suscipit. Blanditiis, assumenda maxime enim perspiciatis alias quae! Accusamus porro minima deserunt dolore! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </div> 
+
+         </div>
+
+           <div>
+
+
+           </div>
 
     </div>
   )
