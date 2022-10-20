b245d1505afdb4d195b19194f6c65512842a5546
unknown <muelinx@gmail.com>
2022-10-20 17:40:03 +0100

    working on the career component

--- src/Components/Career.jsx
+++ src/Components/Career.jsx
@@ -3,25 +3,33 @@ import "./Career.css"
 
 function Career() {
   return (
-    <div>
-
-         <div>
+   <div>
+      <div>
+          <div>
             <h3 id="hcar">CAREER</h3>
               <div class="divcari"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero explicabo accusamus, quisquam aliquam obcaecati excepturi quam aperiam blanditiis neque laborum unde. Fugit commodi dicta impedit saepe dolore, doloremque repellendus dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt eius modi debitis fuga sequi dicta quisquam dolorum suscipit. Blanditiis, assumenda maxime enim perspiciatis alias quae! Accusamus porro minima deserunt dolore! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               </div> 
 
-         </div>
+          </div>
 
-           <div>
-              <h3 id="hcar">EDUCATION</h3>
-              <div class="divcari">
-               Reprehenderit voluptas impedit illo recusandae, esse repellat, velit inventore sit itaque consequuntur ipsam magnam? Maxime eius harum repellat id nesciunt sequi reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ullam expedita quidem assumenda, corrupti maiores, eius laborum ex officia iure enim blanditiis, est dicta? Sit commodi tenetur voluptas minus eius!
+            <div>
+               <h3 id="hcar">EDUCATION</h3>
+               <div class="divcari">
+                Reprehenderit voluptas impedit illo recusandae, esse repellat, velit inventore sit itaque consequuntur ipsam magnam? Maxime  eius harum repellat id nesciunt sequi reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ullam expedita quidem assumenda, corrupti maiores, eius laborum ex officia iure enim blanditiis, est dicta? Sit commodi tenetur voluptas minus eius!
                </div>
            </div>
 
-           
+           <div>
+                <h3 id="hcar">Skills</h3>
+               <div class="divcari">
+                
+               </div>
 
-    </div>
+           </div>
+
+     </div>
+      
+   </div>
   )
 }
 
