b34160694e467ba6e3a7cdb8b6e5a14e67fbcfc4
unknown <muelinx@gmail.com>
2022-10-20 17:58:06 +0100

    working on the career component

--- src/Components/Career.jsx
+++ src/Components/Career.jsx
@@ -3,23 +3,23 @@ import "./Career.css"
 
 function Career() {
   return (
-   <div>
+   <div className='genc'>
       <div className='divsec3'>
-          <div className='div1'>
+          <div className='c1'>
             <h3>CAREER</h3>
               <div> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero explicabo accusamus, quisquam aliquam obcaecati excepturi quam aperiam blanditiis neque laborum unde. Fugit commodi dicta impedit saepe dolore, doloremque repellendus dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt eius modi debitis fuga sequi dicta quisquam dolorum suscipit. Blanditiis, assumenda maxime enim perspiciatis alias quae! Accusamus porro minima deserunt dolore! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               </div> 
 
           </div>
 
-            <div className='div2'>
+            <div className='c2'>
                <h3>EDUCATION</h3>
                <div>
                 Reprehenderit voluptas impedit illo recusandae, esse repellat, velit inventore sit itaque consequuntur ipsam magnam? Maxime  eius harum repellat id nesciunt sequi reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ullam expedita quidem assumenda, corrupti maiores, eius laborum ex officia iure enim blanditiis, est dicta? Sit commodi tenetur voluptas minus eius!
                </div>
            </div>
 
-           <div className='div3'>
+           <div className='c3'>
                 <h3 id="hcar">Skills</h3>
                <div class="divcari">
                 
