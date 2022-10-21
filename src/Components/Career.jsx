e42e83b9fdb6909b4340db5120a0c52a723a8232
unknown <muelinx@gmail.com>
2022-10-21 15:00:52 +0100

    working on the career component

--- src/Components/Career.jsx
+++ src/Components/Career.jsx
@@ -14,14 +14,14 @@ function Career() {
 
             <div className='c2'>
                <h3>EDUCATION</h3>
-               <div className='ci'>
+               <div className='cii'>
                 Reprehenderit voluptas impedit illo recusandae, esse repellat, velit inventore sit itaque consequuntur ipsam magnam? Maxime  eius harum repellat id nesciunt sequi reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ullam expedita quidem assumenda, corrupti maiores, eius laborum ex officia iure enim blanditiis, est dicta? Sit commodi tenetur voluptas minus eius!
                </div>
            </div>
 
            <div className='c3'>
                 <h3 id="hcar">Skills</h3>
-               <div class="divcari">
+               <div className='ciii'>
                 
                </div>
 
