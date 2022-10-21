cc3b7e46a3387323214096ca3f4f1f0d6ae43554
unknown <muelinx@gmail.com>
2022-10-21 15:00:16 +0100

    working on the career component

--- src/Components/Career.jsx
+++ src/Components/Career.jsx
@@ -7,14 +7,14 @@ function Career() {
       <div className='divsec3'>
           <div className='c1'>
             <h3>CAREER</h3>
-              <div className='cii'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero explicabo accusamus, quisquam aliquam obcaecati excepturi quam aperiam blanditiis neque laborum unde. Fugit commodi dicta impedit saepe dolore, doloremque repellendus dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt eius modi debitis fuga sequi dicta quisquam dolorum suscipit. Blanditiis, assumenda maxime enim perspiciatis alias quae! Accusamus porro minima deserunt dolore! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              <div className='ci'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero explicabo accusamus, quisquam aliquam obcaecati excepturi quam aperiam blanditiis neque laborum unde. Fugit commodi dicta impedit saepe dolore, doloremque repellendus dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt eius modi debitis fuga sequi dicta quisquam dolorum suscipit. Blanditiis, assumenda maxime enim perspiciatis alias quae! Accusamus porro minima deserunt dolore! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               </div> 
 
           </div>
 
             <div className='c2'>
                <h3>EDUCATION</h3>
-               <div>
+               <div className='ci'>
                 Reprehenderit voluptas impedit illo recusandae, esse repellat, velit inventore sit itaque consequuntur ipsam magnam? Maxime  eius harum repellat id nesciunt sequi reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ullam expedita quidem assumenda, corrupti maiores, eius laborum ex officia iure enim blanditiis, est dicta? Sit commodi tenetur voluptas minus eius!
                </div>
            </div>
