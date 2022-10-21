48af5f4f9636232fe7634084a822dda85f4ca1c2
unknown <muelinx@gmail.com>
2022-10-21 15:01:08 +0100

    working on the career component

--- src/Components/Career.jsx
+++ src/Components/Career.jsx
@@ -20,9 +20,9 @@ function Career() {
            </div>
 
            <div className='c3'>
-                <h3 id="hcar">Skills</h3>
+                <h3>Skills</h3>
                <div className='ciii'>
-                
+               Reprehenderit voluptas impedit illo recusandae, esse repellat, velit inventore sit itaque consequuntur ipsam magnam? Maxime  eius harum repellat id nesciunt sequi reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ullam expedita quidem assumenda, corrupti maiores, eius laborum ex officia iure enim blanditiis, est dicta? Sit commodi tenetur voluptas minus eius!
                </div>
 
            </div>
