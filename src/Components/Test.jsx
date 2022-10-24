266ceb7a1b18bbef72df58117f4bd6a5e37c0135
unknown <muelinx@gmail.com>
2022-10-24 16:22:26 +0100

    working on the test component

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from './Footer'
-import p from '../Assets/images/'
+import p from '../Assets/images/parrot.jpg'
 import "./Test.css"
 
 
@@ -10,12 +10,12 @@ function Test() {
     <div>
        <div>
             <div>
-                <img src={p} alt="t1" />
+                <img src={p} alt="p1" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt possimus accusamus ipsam! Tempore dolorem iure doloribus sapiente culpa at consectetur nihil blanditiis veniam debitis? Esse modi molestias ducimus cupiditate laborum.</p>
 
             </div>
             <div>
-                <img src="" alt="t2" />
+                <img src="" alt="2" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, similique! Provident beatae praesentium ipsam sed, eos vel autem sint facilis repudiandae aliquid. Esse perspiciatis ut cum beatae, temporibus voluptas placeat?</p>
             </div>
 
