52476804d73201b2a4952d038dfc4c40434d38d4
unknown <muelinx@gmail.com>
2022-10-24 16:51:23 +0100

    working on the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,18 +4,22 @@ import "./Footer.css"
 function Footer() {
   return (
     <div className='genfoot'>
-       <div className='divfoot'>
-             <div>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum explicabo, numquam ipsum sequi voluptatem neque possimus beatae quidem dignissimos quod ipsa repellat voluptas suscipit quis sit accusamus nihil! Eligendi, excepturi.</p>
+        
+       <div className='divf'>
+       <h2>Say hello</h2>
+             <div className='divfoot'>
+              <div>
+                   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum explicabo, numquam ipsum sequi voluptatem neque possimus beatae quidem dignissimos quod ipsa repellat voluptas suscipit quis sit accusamus nihil! Eligendi, excepturi.</p>
 
 
-             </div>
+              </div>
 
-             <div>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum explicabo, numquam ipsum sequi voluptatem neque possimus beatae quidem dignissimos quod ipsa repellat voluptas suscipit quis sit accusamus nihil! Eligendi, excepturi.</p>
+              <div>
+                   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum explicabo, numquam ipsum sequi voluptatem neque possimus beatae quidem dignissimos quod ipsa repellat voluptas suscipit quis sit accusamus nihil! Eligendi, excepturi.</p>
 
                 
-             </div>
+              </div>
+              </div>
 
        </div>
 
