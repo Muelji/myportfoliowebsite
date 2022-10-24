39570eedd9baadeb4b630cb81f79e69dfbae68e8
unknown <muelinx@gmail.com>
2022-10-24 15:29:19 +0100

    working on the test component

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -12,7 +12,7 @@ function Aboutme() {
                   <div className='abouttext'>
                       <div>
                        <h2>About Me</h2>
-                        <p>Hi, My name is Kamshinen Dewan, I am the CEO of Nexus Technology, a tech company in Nigeria. I am a self taught front-end developer. Aside fullstack development, I have an interest in Artificial Intelligence with a particular focus on Machine Learning, Deep Learning, Automation, Robotics and Data Science. At a personal level, I am a man of few words with a reserved, dedicated, and open-minded personality. I like to mind my business in all the things I do. I believe that a person should work on developing themselves with professional skills by learning new things all the time. This has been my guiding principle and philosophy through the years.
+                        <p>Hi, My name is Muelji Damos, I am the CEO of Nexus Technology, a tech company in Nigeria. I am a self taught front-end developer. Aside fullstack development, I have an interest in Artificial Intelligence with a particular focus on Machine Learning, Deep Learning, Automation, Robotics and Data Science. At a personal level, I am a man of few words with a reserved, dedicated, and open-minded personality. I like to mind my business in all the things I do. I believe that a person should work on developing themselves with professional skills by learning new things all the time. This has been my guiding principle and philosophy through the years.
                         </p>
                         <br/><br/>
                         <hr/>
@@ -21,10 +21,10 @@ function Aboutme() {
                            <div>
                            <h2>Contact Details</h2>
                             <address>
-                            Kamshinen Dewan <br/>
-                            Jos, Nigeria <br/>
-                            +2348108644731 <br/>
-                             kjoeldewan@gmail.com
+                            Muelji Damos <br/>
+                            Abuja, Nigeria <br/>
+                            +2348185470198 <br/>
+                            muelinx@gmail.com
                            </address>
                            </div>
                            <div className="downloadcv"><h5>DOWNLOAD CV</h5></div>
