d46a1f48c39d8d0f8f6227c2f9131807c37e6494
unknown <muelinx@gmail.com>
2022-10-24 16:22:08 +0100

    working on the test component

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Footer from './Footer'
+import p from '../Assets/images/'
 import "./Test.css"
+
 
 
 function Test() {
   return (
     <div>
-        <h1>heloo</h1>
+       <div>
+            <div>
+                <img src={p} alt="t1" />
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt possimus accusamus ipsam! Tempore dolorem iure doloribus sapiente culpa at consectetur nihil blanditiis veniam debitis? Esse modi molestias ducimus cupiditate laborum.</p>
+
+            </div>
+            <div>
+                <img src="" alt="t2" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, similique! Provident beatae praesentium ipsam sed, eos vel autem sint facilis repudiandae aliquid. Esse perspiciatis ut cum beatae, temporibus voluptas placeat?</p>
+            </div>
+
+       </div>
       <Footer/>
     </div>
   )
