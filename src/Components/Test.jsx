a2b3726c59917b30b4657f645d3f9f47c3bcfb60
unknown <muelinx@gmail.com>
2022-10-24 15:05:30 +0100

    imported the image files

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import t1 from '../Assets/images/birthday.jpg'
-import t2 from '../Assets/images/birthday.jpg'
-import t3 from '../Assets/images/birthday.jpg'
-import t4 from '../Assets/images/birthday.jpg'
+import t2 from '../Assets/images/blog.jpg'
+import t3 from '../Assets/images/deewanfolior.jpg'
+import t4 from '../Assets/images/reviews.jpg'
 
 import "./Test.css"
 
@@ -15,7 +15,7 @@ function Test() {
          <div>
               <h2>A Few Of My Latest Works</h2>
               <div>
-                 <img src={} alt="t1" /><img src={} alt="t2" /><img src={} alt="t3" /><img src={} alt="t4" />
+                 <img src={t1} alt="t1" /><img src={t2} alt="t2" /><img src={t3} alt="t3" /><img src={t4} alt="t4" />
               </div>
          </div>
          
