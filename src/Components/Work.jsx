0c3d60d15f4cb8d099b780330a8d24f71ddc98f2
unknown <muelinx@gmail.com>
2022-10-26 13:27:34 +0100

    working on footer componenet

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -10,9 +10,22 @@ function Work() {
   return (
     <div className='workdiv'>
        <div className='work'>
-
-
-      </div>
+              <div>
+                  <h2>A Few Of My Latest Works</h2>
+                   <div>
+                     <img src={w1} alt="t1" className='imgd' /><img src={w2} alt="t1" className='imgd' /><img src={w3} alt="t1" className='imgd' /><img src={w4} alt="t1" className='imgd' />
+                  </div>
+              </div>
+         
+             <div className='divcontact'>
+                 <hr id="hr2"/>
+                <h3>Need a place to learn a skill in tech?</h3>
+                <p>
+                I highly recommend BlueHouse Technologies. BlueHouse has a team of skilled and certified experts and is offering to help anyone who would like to start a career in tech. Guaranteed.
+                </p>
+                 <button>Contact Bluehouse</button>
+              </div>
+       </div>
     </div>
   )
 }
