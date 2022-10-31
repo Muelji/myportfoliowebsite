cebca176eaf78970bada330694bdc3b8afb8defb
unknown <muelinx@gmail.com>
2022-10-31 16:16:55 +0100

    working on all component

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -9,14 +9,14 @@ function Test() {
     <div>
     <div className='gentest'>
          <div className='gen1'>
-             <div className='hdiv'>Hear What My Clients Have to Say About my Works</div>
+             <div className='hdiv'><h3>Hear What My Clients Have to Say About my Works</h3></div>
              <div className='divtest'>
-                  <div>
+                  <div className='test1'>
                       <img src={p} alt="p1" className='imgp' />
                       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt possimus accusamus ipsam! Tempore dolorem iure doloribus sapiente culpa at consectetur nihil blanditiis veniam debitis? Esse modi molestias ducimus cupiditate laborum.</p>
 
                   </div>
-                  <div>
+                  <div className='test1'>
                      <img src={p} alt="p2" className='imgp' />
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, similique! Provident beatae praesentium ipsam sed, eos vel autem sint facilis repudiandae aliquid. Esse perspiciatis ut cum beatae, temporibus voluptas placeat?</p>
                   </div>
